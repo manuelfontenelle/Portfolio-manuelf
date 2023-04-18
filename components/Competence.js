48a10d7ab3fff8fd38fd32b073bb8b3b99b44204
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-04-18 20:12:51 +0200

    fix css

--- components/Competence.js
+++ components/Competence.js
@@ -2,15 +2,14 @@ import React from "react"
 
 const Competence = () => {
 	return (
-		<div className="w-[100%] xl:w-[48%] h-[350px] bg-[#101D43] dark:bg-[#A8A3A0] rounded-md p-6 flex flex-col gap-16 relative ">
-			<div className=" bg-[#DCDAD9] dark:bg-[#222222] w-16 h-16 rounded-md"></div>
-			<div className="absolute bottom-6">
-				<span className="font-SofiaSans uppercase text-xl">front-end</span>
-				<span className=" block text-xs mt-2 pr-6">
+		<div className="w-[100%] xl:w-[48%] h-[450px] bg-[#101D43] dark:bg-[#A8A3A0] rounded-md p-8 flex flex-col gap-16 relative ">
+			<div className=" bg-[#DCDAD9] dark:bg-[#222222] w-20 h-20 rounded-md"></div>
+			<div className="absolute bottom-8">
+				<span className="font-SofiaSans uppercase text-3xl">front-end</span>
+				<span className=" block text-base leading-5 font-medium mt-5 pr-8 text-[#DCDAD9] dark:text-[#222222]">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 					eiusmod tempor incidid enim ad minim veniam, quis nostrud
-					exercitationlamco. ectetur adipiscing elit, sed do eiusmod tempor
-					incididunt ut labore et dolore. <br />
+					exercitationlamco. <br />
 					<br />
 					ectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
 					dolore.
