28de91aa45980edf20b4de0a536504a66225bb7a
manuelfontenelle <manuel.fontenelle@gmail.com>
2023-05-29 20:21:09 +0200

    Back to top

--- components/Competence.js
+++ components/Competence.js
@@ -1,21 +1,21 @@
-import React from "react"
+import React from "react";
 
 const Competence = ({ title, svg, desc1, desc2 }) => {
-	return (
-		<div className="w-[100%] xl:w-[48%] xl:h-[500px] 2xl:h-[470px] bg-[#101D43] dark:bg-[#A8A3A0] rounded-md p-8 flex flex-col gap-8 relative ">
-			<div className=" bg-[#DCDAD9] dark:bg-[#222222] w-16 h-16 rounded-md flex justify-center items-center">
-				{svg}
-			</div>
-			<div className="">
-				<span className="font-SofiaSans uppercase text-3xl">{title}</span>
-				<span className=" block text-sm sm:text-base leading-5 font-medium mt-2  text-[#DCDAD9] dark:text-[#222222]">
-					{desc1} <br />
-					<br />
-					{desc2}
-				</span>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="w-[100%] xl:w-[48%] xl:h-[535px] 2xl:h-[470px] bg-[#101D43] dark:bg-[#A8A3A0] rounded-md p-6 flex flex-col gap-8 relative ">
+      <div className=" bg-[#DCDAD9] dark:bg-[#222222] w-16  rounded-md flex justify-center items-center">
+        {svg}
+      </div>
+      <div className="">
+        <span className="font-SofiaSans uppercase text-3xl">{title}</span>
+        <span className=" block  text-sm sm:text-base  font-medium mt-2  text-[#DCDAD9] dark:text-[#222222]">
+          {desc1} <br />
+          <br />
+          {desc2}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Competence
+export default Competence;
--- pages/projects/marvel.js
+++ pages/projects/marvel.js
@@ -1,105 +1,105 @@
-import Head from "next/head"
-import Image from "next/image"
-import { Inter } from "@next/font/google"
-import styles from "/styles/Home.module.css"
-import NavBar from "/components/project/NavBar"
-import Intro from "/components/project/Intro"
-import About from "../../components/project/About"
-import Gallery from "../../components/project/Gallery"
-import NavProject from "../../components/project/NavProject"
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "/styles/Home.module.css";
+import NavBar from "/components/project/NavBar";
+import Intro from "/components/project/Intro";
+import About from "../../components/project/About";
+import Gallery from "../../components/project/Gallery";
+import NavProject from "../../components/project/NavProject";
 
-import Footer from "/components/Footer"
-import BackToTop from "/components/BackToTop"
-import marvel1 from "../../public/projects/marvel1.jpg"
-import marvel2 from "../../public/projects/marvel2.png"
-import marvel3 from "../../public/projects/marvel3.jpg"
-import marvel4 from "../../public/projects/marvel4.png"
+import Footer from "/components/Footer";
+import BackToTop from "/components/BackToTop";
+import marvel1 from "../../public/projects/marvel1.jpg";
+import marvel2 from "../../public/projects/marvel2.png";
+import marvel3 from "../../public/projects/marvel3.jpg";
+import marvel4 from "../../public/projects/marvel4.png";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function DomaineAventurier() {
-	return (
-		<>
-			<Head>
-				<title>Project | Marvel Library</title>
-				<meta
-					name="description"
-					content="Marvel Library - Fullstack React js | Characters / Comics / Favorites"
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.png" />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Project | Marvel Library</title>
+        <meta
+          name="description"
+          content="Marvel Library - Fullstack React js | Characters / Comics / Favorites"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
 
-			<main className=" dark:bg-[#DCDAD9] bg-[#27e193] overflow-x-hidden ">
-				<section id="introProject" className="pt-14 pb-8 md:pb-16">
-					<header className="bg-[#27e193] dark:bg-[#DCDAD9] pt-5 z-20">
-						<div className="container">
-							<NavBar />
-						</div>
-					</header>
-					<Intro
-						img1={marvel1}
-						descAlt={"Marvel"}
-						objectPosition={"objectPositionLeft27"}
-						titreIntro="Marvel Library"
-						textIntro="Full Stack React Js Web App"
-						urlIntro="marvel-manuelf.netlify.app"
-						urlDetailIntro="https://marvel-manuelf.netlify.app/"
-					/>
-				</section>
-				<section
-					id="aboutProject"
-					className="pb-8 md:pb-16 pt-8 dark:bg-[#222222] bg-[#213B86] "
-				>
-					<About
-						desc1={"Marvel Library web App realisation"}
-						desc2={" React Js | Node Js | Call Api "}
-						client={"Marvel"}
-						role={"Web Developer"}
-						year={"2023"}
-						country={"France"}
-					/>
-				</section>
-				<section
-					id="gallery"
-					className="pb-8 md:pb-16 pt-8 dark:bg-[#222222] bg-[#213B86] "
-				>
-					<Gallery
-						img2={marvel2}
-						img3={marvel3}
-						img4={marvel4}
-						paddingBlock1={""}
-						paddingBlock2={""}
-						paddingBlock3={"lg:py-10"}
-						object1={"object-contain"}
-						object2={"object-cover"}
-						object3={"object-contain"}
-						descAlt={"Marc Fontenelle"}
-					/>
-				</section>
+      <main className=" dark:bg-[#DCDAD9] bg-[#27e193] overflow-x-hidden ">
+        <section id="introProject" className="pt-14 pb-8 md:pb-16">
+          <header className="bg-[#27e193] dark:bg-[#DCDAD9] pt-5 z-20">
+            <div className="container">
+              <NavBar />
+            </div>
+          </header>
+          <Intro
+            img1={marvel1}
+            descAlt={"Marvel"}
+            objectPosition={"objectPositionLeft27"}
+            titreIntro="Marvel Library"
+            textIntro="Full Stack React Js Web App"
+            urlIntro="marvel-manuelf.netlify.app"
+            urlDetailIntro="https://marvel-manuelf.netlify.app/"
+          />
+        </section>
+        <section
+          id="aboutProject"
+          className="pb-8 md:pb-16 pt-8 dark:bg-[#222222] bg-[#213B86] "
+        >
+          <About
+            desc1={"Marvel Library web App realisation"}
+            desc2={" React Js | Node Js | Call Api "}
+            client={"Marvel"}
+            role={"Web Developer"}
+            year={"2023"}
+            country={"France"}
+          />
+        </section>
+        <section
+          id="gallery"
+          className="pb-8 md:pb-16 pt-8 dark:bg-[#222222] bg-[#213B86] "
+        >
+          <Gallery
+            img2={marvel2}
+            img3={marvel3}
+            img4={marvel4}
+            paddingBlock1={""}
+            paddingBlock2={""}
+            paddingBlock3={"lg:py-10"}
+            object1={"object-contain"}
+            object2={"object-cover"}
+            object3={"object-contain"}
+            descAlt={"Marvel Library"}
+          />
+        </section>
 
-				<section
-					id="navProject"
-					className=" pt-4 md:pt-8 dark:bg-[#222222] bg-[#213B86] "
-				>
-					<NavProject
-						prevUrl="/projects/vinted"
-						nextUrl="/projects/marcFontenelle"
-					/>
-				</section>
+        <section
+          id="navProject"
+          className=" pt-4 md:pt-8 dark:bg-[#222222] bg-[#213B86] "
+        >
+          <NavProject
+            prevUrl="/projects/vinted"
+            nextUrl="/projects/marcFontenelle"
+          />
+        </section>
 
-				<section
-					id="Footer"
-					className="pb-8 md:pb-16  dark:bg-[#222222] bg-[#213B86]"
-				>
-					<div className="container">
-						<Footer />
-					</div>
-				</section>
-				<section id="BackToTop" className="dark:bg-[#DCDAD9] bg-[#213B86] ">
-					<BackToTop />
-				</section>
-			</main>
-		</>
-	)
+        <section
+          id="Footer"
+          className="pb-8 md:pb-16  dark:bg-[#222222] bg-[#213B86]"
+        >
+          <div className="container">
+            <Footer />
+          </div>
+        </section>
+        <section id="BackToTop" className="dark:bg-[#DCDAD9] bg-[#213B86] ">
+          <BackToTop />
+        </section>
+      </main>
+    </>
+  );
 }
