0f8872fbf3e04f4326a9b541db3e83a884b62795
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-06-06 15:52:30 +0200

    scroll top smooth

--- components/BackToTop.js
+++ components/BackToTop.js
@@ -1,12 +1,17 @@
 import React from "react"
 import BackToTopElem from "./BackToTopElem"
 const BackToTop = () => {
+	const scrollTop = () => {
+		window.scrollTo({ top: 0, behavior: "smooth" })
+	}
+
 	return (
 		<div>
 			<div className="border-t-[1px] border-[#DCDAD9] dark:border-[#222222] hover:dark:border-[#DCDAD9] text-[#DCDAD9] hover:text-[#213B86] hover:dark:text-[#dcdad9] dark:text-[#222222] dark:hidden backToTopHover">
-				<a
+				<div
+					onClick={scrollTop}
 					href="#"
-					className="flex justify-start items-center font-SofiaSans text-[5vw] md:text-[2.5vw] uppercase ml-[0%] leading-none whitespace-nowrap tracking-[-0.03em]"
+					className="cursor-pointer flex justify-start items-center font-SofiaSans text-[5vw] md:text-[2.5vw] uppercase ml-[0%] leading-none whitespace-nowrap tracking-[-0.03em]"
 				>
 					<div className="messageDefilantHover ml-[-40px] p-4">
 						<BackToTopElem></BackToTopElem>
@@ -74,12 +79,12 @@ const BackToTop = () => {
 						<BackToTopElem></BackToTopElem>
 						<BackToTopElem></BackToTopElem>
 					</div>
-				</a>
+				</div>
 			</div>
 			<div className="border-t-[1px] border-[#DCDAD9] dark:border-[#222222] hover:dark:border-[#DCDAD9] text-[#DCDAD9] hover:text-[#213B86] hover:dark:text-[#dcdad9] dark:text-[#222222] hidden dark:block backToTopHoverDark">
-				<a
-					href="#"
-					className="flex justify-start items-center font-SofiaSans text-[5vw] md:text-[2.5vw] uppercase ml-[0%] leading-none whitespace-nowrap tracking-[-0.03em]"
+				<div
+					onClick={scrollTop}
+					className="cursor-pointer flex justify-start items-center font-SofiaSans text-[5vw] md:text-[2.5vw] uppercase ml-[0%] leading-none whitespace-nowrap tracking-[-0.03em]"
 				>
 					<div className="messageDefilantHover ml-[-40px] p-4">
 						<BackToTopElem></BackToTopElem>
@@ -147,7 +152,7 @@ const BackToTop = () => {
 						<BackToTopElem></BackToTopElem>
 						<BackToTopElem></BackToTopElem>
 					</div>
-				</a>
+				</div>
 			</div>
 		</div>
 	)
